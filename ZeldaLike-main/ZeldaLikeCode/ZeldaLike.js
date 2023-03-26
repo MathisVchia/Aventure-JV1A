ac5c5d66aabee384535518049c39b90a5be1c311
MathisVchia <118368064+MathisVchia@users.noreply.github.com>
2023-03-26 23:08:16 +0200

    menu

--- ZeldaLike-main/ZeldaLikeCode/ZeldaLike.js
+++ ZeldaLike-main/ZeldaLikeCode/ZeldaLike.js
@@ -1,3 +1,43 @@
+class Menu extends Phaser.Scene {
+    
+    constructor(){
+        super("Menu");
+        this.start;
+        this.boutonStart; // déclarer la variable ici
+    }
+
+    preload(){
+        this.load.image('MenuFond', 'assets/MenuFond.png');
+        this.load.image('BoutonStart', 'assets/BoutonStart.png');
+    }
+
+    create(){
+        // ajouter l'image de fond
+        this.add.image(0, 0, 'MenuFond').setOrigin(0, 0);
+
+        
+        // créer le bouton "BoutonStart" et l'ajouter à la scène
+        this.boutonStart = this.add.image(200, 400, 'BoutonStart').setOrigin(0, 0);
+
+        // ajouter l'interaction avec le bouton ici
+        this.boutonStart.setInteractive();
+        this.boutonStart.on('pointerdown', () => {
+            this.startGame();
+        });
+
+        
+    }
+
+    startGame(){
+        this.scene.start('chambre');
+    }
+}
+
+
+
+
+
+
 class ZeldaLike extends Phaser.Scene {
 
     constructor() {
@@ -454,7 +494,7 @@ var config = {
             debug: true
         }
     },
-    scene: [ZeldaLike, level2],
+    scene: [Menu, ZeldaLike, level2],
 };
 
 new Phaser.Game(config);
